c6bee532a13268d3875f1cb3770376b1bcd041f3
sin <sin@vinova.com.sg>
2021-07-04 15:07:14 +0700

    [UI] - fix UI responsive

--- src/App.tsx
+++ src/App.tsx
@@ -13,14 +13,16 @@ function App() {
   return (
     <Router>
       <div className="App">
-        <div className="App--imgage">
-          <a href="https://hellohealthgroup.com/vietnam/" target="_blank">
-            <img alt="hello-health" src="https://hellohealthgroup.com/app/themes/hhg/public/img/site-logo.png"/>
-          </a>
-        </div>
-        <div className="App--menu">
-          <Link to="/assignment1">Assignment1</Link>
-          <Link to="/assignment2">Assignment2</Link>
+        <div className="App--header">
+          <div className="App--image">
+            <a href="https://hellohealthgroup.com/vietnam/" target="_blank">
+              <img alt="hello-health" src="https://hellohealthgroup.com/app/themes/hhg/public/img/site-logo.png"/>
+            </a>
+          </div>
+          <div className="App--menu">
+            <Link to="/assignment1">Assignment1</Link>
+            <Link to="/assignment2">Assignment2</Link>
+          </div>
         </div>
         <Routes/>
       </div>
